420b9eecda92c1b121a975b27578a3f7ca5a0b97
Ahnaf Farhan Hossain <ahnaffarhanhossain@gmail.com>
2025-02-13 17:05:31 +0600

    Update phone number in footer component

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
               <a href="mailto:ahnaffarhanhossain@gmail.com">
                 <i className="ri-mail-line" /> ahnaffarhanhossain@gmail.com
               </a>
-              <a href="tel:+8801756029009">
+              <a href="tel:+88018861554469">
                 <i className="ri-phone-line" /> +880 1886 155446
               </a>
             </div>
