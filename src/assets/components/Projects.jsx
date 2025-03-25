bddb0fa408c5cf332cc9b966a6d92ef8e08827f0
Ahnaf Farhan Hossain <ahnaffarhanhossain@gmail.com>
2025-03-25 16:52:03 +0600

    Add Feedlytic project card to Projects component

--- src/assets/components/Projects.jsx
+++ src/assets/components/Projects.jsx
@@ -6,6 +6,7 @@ import Elecar from "../images/elecar.webp";
 import Healthy from "../images/Healthy.webp";
 import LaPizzaPopolare from "../images/la-pizza-popolare.webp";
 import GeminiClone from "../images/gemini-clone.webp";
+import FeedlyticCover from "../images/Feedlytic-cover.webp"
 import "../styles/projects.css";
 import { useRef } from "react";
 
@@ -60,6 +61,42 @@ const Projects = () => {
           </button>
         </div>
         <div className="projects-wrapper" ref={sliderRef}>
+          {/* Project: Feedlytic - Client Feedback Management Portal */}
+          <div className="card">
+            <div className="card-top">
+              <h2>Feedlytic - Client Feedback Management Portal</h2>
+              <a href="https://feedlytic-ahnaf.vercel.app" target="_blank">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={16}
+                  height={16}
+                  fill="currentColor"
+                  className="bi bi-box-arrow-up-right"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a.5.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
+                  />
+                </svg>
+              </a>
+            </div>
+            <p>
+              Feedlytic is a client-centric feedback management system that streamlines the collection, analysis, and visualization of customer insights.
+            </p>
+            <div className="tech-stack">
+              <span>React JS</span>
+              <span>Next JS</span>
+              <span>NextAuth</span>
+              <span>Tailwind CSS</span>
+              <span>Supabase</span>
+            </div>
+            <img src={FeedlyticCover} alt="Shadient Website" />
+          </div>
           {/* Project: Shadient - Lead Gen. Agency */}
           <div className="card">
             <div className="card-top">
